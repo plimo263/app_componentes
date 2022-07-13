25f4b9bb081ac3ff814a49b31022be402857e615
Marcos Felipe <plimo263@gmail.com>
2022-07-13 05:36:50 -0300

    Prevenindo referencia nula em TAB

--- src/components/tab.js
+++ src/components/tab.js
@@ -4,66 +4,66 @@ import { Box, Container, Tab as Aba, Tabs } from '@mui/material';
 
 // Componente aba
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box>
-            {children}
-          </Box>
-        )}
-      </div>
-    );
-  }
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 
 const Tab = ({ indice, cabeInativo, setIndice, corpo, cabe, verBotaoControle }) => {
-    const [pagina, setPagina ] = useState(indice);
-    // Funcao para manipular os indices
-    const fnIndice = useCallback((valor)=>{
-      setPagina(valor);
-      setIndice(valor);
-    }, [setPagina, setIndice]);
-    
-    return (
-        <Container disableGutters disablePadding maxWidth={false} >
-            
-            <Tabs 
-              variant="scrollable"
-              scrollButtons
-              allowScrollButtonsMobile={verBotaoControle}
-              
-              value={pagina} onChange={(evt, valor)=> fnIndice(valor) }>
-                {cabe.map((ele,idx)=>(
-                    <Aba disabled={cabeInativo.includes(idx)} label={ele} key={idx} {...a11yProps(idx)} />
-                ))}
-            </Tabs>
-            
-            {corpo.map((ele,idx)=>(
-                <TabPanel value={pagina} key={idx} index={idx}>
-                    {ele}                
-                </TabPanel>
-            ))}
-        </Container>
-    ) 
+  const [pagina, setPagina] = useState(indice);
+  // Funcao para manipular os indices
+  const fnIndice = useCallback((valor) => {
+    setPagina(valor);
+    setIndice(valor);
+  }, [setPagina, setIndice]);
+
+  return (
+    <Container disableGutters disablePadding maxWidth={false} >
+
+      <Tabs
+        variant="scrollable"
+        scrollButtons
+        allowScrollButtonsMobile={verBotaoControle}
+
+        value={pagina} onChange={(evt, valor) => fnIndice(valor)}>
+        {cabe.map((ele, idx) => (
+          <Aba disabled={cabeInativo?.includes(idx)} label={ele} key={idx} {...a11yProps(idx)} />
+        ))}
+      </Tabs>
+
+      {corpo.map((ele, idx) => (
+        <TabPanel value={pagina} key={idx} index={idx}>
+          {ele}
+        </TabPanel>
+      ))}
+    </Container>
+  )
 }
 //
 Tab.defaultProps = {
   indice: 0,
-  setIndice: ()=>{},
+  setIndice: () => { },
   cabeInativo: [],
 }
 //
