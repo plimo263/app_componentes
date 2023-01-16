aa81374da4d8bacea1a00bb885bd2aa3e69e1dfb
Marcos Felipe <plimo263@gmail.com>
2023-01-16 20:36:31 -0300

    Adicionando recurso para que o calendario possa exibir uma div integrada lateralmente quando clicado

--- src/components/calendario.js
+++ src/components/calendario.js
@@ -1,117 +1,240 @@
-import React, { memo, useCallback, useState } from 'react';
-import PropTypes from 'prop-types';
-import _ from 'lodash';
-import { addDays, subDays, eachDayOfInterval, startOfMonth, endOfMonth, getDay, subMonths, addMonths, parseISO, format } from 'date-fns';
-import { Button, useMediaQuery, useTheme, Stack, Paper, Container, CircularProgress } from '@mui/material';
-import { H6, Caption } from './tipografia';
+import React, { memo, useCallback, useState } from "react";
+import PropTypes from "prop-types";
+import _ from "lodash";
+import {
+  addDays,
+  subDays,
+  eachDayOfInterval,
+  startOfMonth,
+  endOfMonth,
+  getDay,
+  subMonths,
+  addMonths,
+  parseISO,
+  format,
+} from "date-fns";
+import {
+  Button,
+  useMediaQuery,
+  useTheme,
+  Stack,
+  Paper,
+  Container,
+  CircularProgress,
+  Grid,
+  Grow,
+} from "@mui/material";
+import { H6, Caption } from "./tipografia";
 // Icones
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import { ptBR } from 'date-fns/locale';
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { ptBR } from "date-fns/locale";
+import { useToggle } from "react-use";
+import { AnimatePresence, motion } from "framer-motion";
 
 //
 const sxCol = {
-  width: 'calc(14.28% - 16px)',
+  width: "calc(14.28% - 16px)",
   p: 1,
   borderRadius: 0,
-}
+};
 //
 const nomeDias = [
-  'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'
-]
+  "Domingo",
+  "Segunda",
+  "Terça",
+  "Quarta",
+  "Quinta",
+  "Sexta",
+  "Sábado",
+];
 
-const diaDehoje = format(new Date(), 'yyyy-MM-dd');
+const diaDehoje = format(new Date(), "yyyy-MM-dd");
 
-const Calendario = ({ aguardar, onControleCalendario, sxCabe, sxCorpo, dataInicial, render, renderEstiloDia, onClick })=> {
-  const [mesAtual, setMesAtual ] = useState(dataInicial);
-  const { recuarMes, avancarMes, diasCalendario, ultimosDiasMesAnterior, primeirosDiasMesPosterior } = gerarDiasCalendario(mesAtual);
+const Calendario = ({
+  aguardar,
+  onControleCalendario,
+  sxCabe,
+  sxCorpo,
+  dataInicial,
+  render,
+  renderEstiloDia,
+  onClick,
+  renderModalLateral,
+}) => {
+  const [mesAtual, setMesAtual] = useState(dataInicial);
+  const [diaSelecionado, setDiaSelecionado] = useState(null);
+  //
+  const {
+    recuarMes,
+    avancarMes,
+    diasCalendario,
+    ultimosDiasMesAnterior,
+    primeirosDiasMesPosterior,
+  } = gerarDiasCalendario(mesAtual);
   // Veja se é mobile
-  const isMobile = useMediaQuery( useTheme()?.breakpoints?.down('md') );
+  const isMobile = useMediaQuery(useTheme()?.breakpoints?.down("md"));
   // Alterando para o proximo (ou anterior) mês
-  const fnRecuar = useCallback(()=>{
+  const fnRecuar = useCallback(() => {
     // Formata para pegar  o primeiro e ultimo dia do mes posterior
-    if(onControleCalendario){
-      const priDia = format(startOfMonth(parseISO(recuarMes)), 'yyyy-MM-dd');
-      const ultDia = format(endOfMonth(parseISO(recuarMes)), 'yyyy-MM-dd');
-      onControleCalendario(priDia, ultDia, 'recuar');
+    if (onControleCalendario) {
+      const priDia = format(startOfMonth(parseISO(recuarMes)), "yyyy-MM-dd");
+      const ultDia = format(endOfMonth(parseISO(recuarMes)), "yyyy-MM-dd");
+      onControleCalendario(priDia, ultDia, "recuar");
     }
-    
+
     setMesAtual(recuarMes); // primeiro dia do mes anterior
   }, [recuarMes, onControleCalendario]);
-  const fnAvancar = useCallback(()=>{
+  const fnAvancar = useCallback(() => {
     // Formata para pegar  o primeiro e ultimo dia do mes posterior
-    if(onControleCalendario){
-      const priDia = format(startOfMonth(parseISO(avancarMes)), 'yyyy-MM-dd');
-      const ultDia = format(endOfMonth(parseISO(avancarMes)), 'yyyy-MM-dd');
-      onControleCalendario(priDia, ultDia, 'avancar');
+    if (onControleCalendario) {
+      const priDia = format(startOfMonth(parseISO(avancarMes)), "yyyy-MM-dd");
+      const ultDia = format(endOfMonth(parseISO(avancarMes)), "yyyy-MM-dd");
+      onControleCalendario(priDia, ultDia, "avancar");
     }
     //
     setMesAtual(avancarMes); // primeiro dia do mes posterior
-  }, [avancarMes, onControleCalendario])
-  
+  }, [avancarMes, onControleCalendario]);
+  // Funcao para selecionar dia
+  const fnSelecionarDia = useCallback(
+    (diaFormatado, desabilitar) => {
+      if (!desabilitar && !aguardar) onClick(diaFormatado);
+      // Verifica se tem o renderModalLateral
+      if (renderModalLateral) {
+        // Se o dia ja tiver sido selecionado vai ocultar o dia selecionado para recolher o modal lateral
+        if (diaFormatado === diaSelecionado) {
+          setDiaSelecionado(null);
+        } else {
+          // Defina o dia formatado para que o mesmo acione o renderModalLateral para exibir o modal lateral
+          setDiaSelecionado(diaFormatado);
+        }
+      }
+    },
+    [aguardar, onClick, renderModalLateral, diaSelecionado, setDiaSelecionado]
+  );
+
   return (
-    <Container disableGutters maxWidth='lg'>
-      <Stack direction='row' justifyContent='space-between'>
-        <Button disabled={aguardar} onClick={fnRecuar} title='Mês anterior' startIcon={aguardar ? <CircularProgress size={20} /> : <ArrowBackIosIcon />}>
-         {!isMobile && 'Recuar'}
-        </Button>
-        <H6>{_.capitalize( format(parseISO(mesAtual), 'MMMM', { locale: ptBR })) } - {format(parseISO(mesAtual), 'yyyy', { locale: ptBR })}</H6>
-        <Button disabled={aguardar} onClick={fnAvancar} title='Próximo mês' endIcon={aguardar ? <CircularProgress size={20} /> : <ArrowForwardIosIcon />}>
-          {!isMobile && 'Avançar'}
-        </Button>
+    <Container disableGutters maxWidth="lg">
+      <Stack>
+        <Stack direction="row" justifyContent="space-between">
+          <Button
+            disabled={aguardar}
+            onClick={fnRecuar}
+            title="Mês anterior"
+            startIcon={
+              aguardar ? <CircularProgress size={20} /> : <ArrowBackIosIcon />
+            }
+          >
+            {!isMobile && "Recuar"}
+          </Button>
+          <H6>
+            {_.capitalize(format(parseISO(mesAtual), "MMMM", { locale: ptBR }))}{" "}
+            - {format(parseISO(mesAtual), "yyyy", { locale: ptBR })}
+          </H6>
+          <Button
+            disabled={aguardar}
+            onClick={fnAvancar}
+            title="Próximo mês"
+            endIcon={
+              aguardar ? (
+                <CircularProgress size={20} />
+              ) : (
+                <ArrowForwardIosIcon />
+              )
+            }
+          >
+            {!isMobile && "Avançar"}
+          </Button>
+        </Stack>
+        <Stack direction="row">
+          <AnimatePresence>
+            {diaSelecionado && renderModalLateral && (
+              <motion.div
+                initial={{ width: 0 }}
+                animate={{ width: "33.33%", transition: { duration: 0.2 } }}
+                exit={{ width: 0.01, transition: { duration: 0.1 } }}
+                key="modalLateral"
+              >
+                {renderModalLateral(diaSelecionado)}
+              </motion.div>
+            )}
+            <Stack flex={1}>
+              <Stack direction="row">
+                {nomeDias.map((ele, idx) => (
+                  <CalendarioHeader
+                    key={idx}
+                    isMobile={isMobile}
+                    ele={ele}
+                    sxCol={sxCol}
+                    sxCabe={sxCabe}
+                  />
+                ))}
+              </Stack>
+              <Stack direction="row" flexWrap="wrap">
+                {diasCalendario?.map((ele, idx) => {
+                  const diaFormatado = format(ele, "yyyy-MM-dd");
+                  const diaNumero = format(ele, "dd");
+                  // Veja se o dia é o dia de hoje
+                  const isHoje = diaDehoje === diaFormatado;
+                  // Verifica se o dia em questão faz parte dos dias que devem ser "desabilitados"
+                  const desabilitar =
+                    primeirosDiasMesPosterior?.includes(ele) ||
+                    ultimosDiasMesAnterior?.includes(ele);
+                  const sxEstiloDia = renderEstiloDia
+                    ? renderEstiloDia(diaFormatado)
+                    : {};
 
+                  return (
+                    <Paper
+                      onClick={() => fnSelecionarDia(diaFormatado, desabilitar)}
+                      key={idx}
+                      sx={{ ...sxCol, ...sxCorpo, ...sxEstiloDia }}
+                    >
+                      <Stack alignItems="flex-start" spacing={1}>
+                        <Caption
+                          title="Hoje"
+                          sx={{ typography: isHoje ? "h6" : "caption" }}
+                          fontWeight={isHoje ? "bold" : "normal"}
+                          color={
+                            isHoje
+                              ? "primary.main"
+                              : desabilitar
+                              ? "text.disabled"
+                              : ""
+                          }
+                        >
+                          {diaNumero}
+                        </Caption>
+                        {render && render(diaFormatado)}
+                      </Stack>
+                    </Paper>
+                  );
+                })}
+              </Stack>
+            </Stack>
+          </AnimatePresence>
+        </Stack>
       </Stack>
-      <Stack direction='row'>
-        {nomeDias.map((ele,idx)=>(
-          <CalendarioHeader key={idx} 
-            isMobile={isMobile}
-            ele={ele}
-            sxCol={sxCol}
-            sxCabe={sxCabe}
-
-          />
-        ))}
-      </Stack>
-      <Stack direction='row' flexWrap='wrap'>
-      {diasCalendario?.map((ele,idx)=>{
-        const diaFormatado = format(ele, 'yyyy-MM-dd');
-        const diaNumero = format(ele, 'dd');
-        // Veja se o dia é o dia de hoje
-        const isHoje = diaDehoje === diaFormatado;
-        // Verifica se o dia em questão faz parte dos dias que devem ser "desabilitados"
-        const desabilitar = primeirosDiasMesPosterior?.includes(ele) || ultimosDiasMesAnterior?.includes(ele);
-        const sxEstiloDia = renderEstiloDia ? renderEstiloDia(diaFormatado) : {};
-        
-        return (
-        <Paper onClick={()=> !desabilitar && !aguardar && onClick(diaFormatado) } key={idx} sx={{...sxCol, ...sxCorpo, ...sxEstiloDia }}>
-          <Stack alignItems='flex-start' spacing={1}>
-              <Caption title='Hoje' sx={{ typography : isHoje ? 'h6' : 'caption' }} fontWeight={isHoje ? 'bold' : 'normal'} color={isHoje ? 'primary.main' : desabilitar ? 'text.disabled' : ''}>{diaNumero}</Caption>
-              {render && render(diaFormatado)}
-          </Stack>
-        </Paper>
-        )
-      })}
-    </Stack>
     </Container>
-  )
-}
+  );
+};
 // Cabecalho do calendario
-const CalendarioHeader = memo(({ isMobile, ele, sxCol, sxCabe })=>(
-  <Paper sx={{...sxCol, ...sxCabe}}>
+const CalendarioHeader = memo(({ isMobile, ele, sxCol, sxCabe }) => (
+  <Paper sx={{ ...sxCol, ...sxCabe }}>
     <H6> {isMobile ? ele.substring(0, 1) : ele} </H6>
   </Paper>
-))
+));
 
 Calendario.defaultProps = {
   sxCabe: {
-    backgroundColor: theme=> theme.palette.secondary.main,
-    color: theme=> theme.palette.secondary.contrastText,
+    backgroundColor: (theme) => theme.palette.secondary.main,
+    color: (theme) => theme.palette.secondary.contrastText,
   },
   sxCorpo: {
-    height: '72px'
+    height: "72px",
   },
-  onClick: ()=>{}
-}
+  onClick: () => {},
+};
 // Definindo valor das propriedades default
 Calendario.propTypes = {
   /** valor props sx que pode ser passada para formatar o cabecalho */
@@ -130,54 +253,64 @@ Calendario.propTypes = {
   onControleCalendario: PropTypes.func,
   /** Uma props que trava qualquer interatividade do calendario quando ativa */
   aguardar: PropTypes.bool,
-
-}
+  /** Uma funcao de callback que receberá o dia e possibilitará ao usuario retornar um componente que vai representar o conteudo dentro do modal lateral que vai abrir ao ser selecionado. Caso o dia volte a ser clicado o modalLateral volta a se fechar. EX: (dia)=>(<div>O dia é dia</div>) */
+  renderModalLateral: PropTypes.func,
+};
 
 // Recebe a data e cria o calendario com os dias corretos
-const gerarDiasCalendario = (anoMesDia)=>{
+const gerarDiasCalendario = (anoMesDia) => {
+  // Registra o mes atual
+  const anoMesAtual = parseISO(anoMesDia);
 
-    // Registra o mes atual
-    const anoMesAtual = parseISO(anoMesDia);
+  const arr = eachDayOfInterval({
+    start: startOfMonth(anoMesAtual),
+    end: endOfMonth(anoMesAtual),
+  });
+  // Pega o primeiro dia e veja qual dia ele é na semana
+  const primeiroDiaMes = getDay(arr[0]);
+  const ultimoDiaMes = getDay(arr[arr.length - 1]);
+  // Dias do mes anterior
+  let ultimosDiasMesAnterior;
+  let primeirosDiasMesPosterior;
 
-    const arr = eachDayOfInterval({start: startOfMonth(anoMesAtual),  end: endOfMonth(anoMesAtual) });
-    // Pega o primeiro dia e veja qual dia ele é na semana
-    const primeiroDiaMes = getDay(arr[0]);
-    const ultimoDiaMes = getDay(arr[arr.length - 1]);
-    // Dias do mes anterior
-    let ultimosDiasMesAnterior;
-    let primeirosDiasMesPosterior;
+  // Verifica se o primeiro dia do mes é maior que 0
+  if (primeiroDiaMes > 0) {
+    // Recue pela quantidade de dias da semana que avançaram
+    const primeiroDiaSemana = subDays(arr[0], primeiroDiaMes);
 
-    // Verifica se o primeiro dia do mes é maior que 0
-    if(primeiroDiaMes > 0){
-        // Recue pela quantidade de dias da semana que avançaram
-        const primeiroDiaSemana = subDays(arr[0], primeiroDiaMes);
-        
-        // Pega os dias até (mas não) a data do primeiro dia do mês
-        ultimosDiasMesAnterior = eachDayOfInterval({ start: primeiroDiaSemana, end: subDays(arr[0], 1) });
-    }
-    //  Veja se o ultimo dia do mes é menor que 6
-    if(ultimoDiaMes < 6){
-        const ultimoDiaDaSemana = addDays(arr[ arr.length - 1 ], 6 - ultimoDiaMes);
-        // Pega os dias até (mas não) a data do ultimo dia do mes
-        primeirosDiasMesPosterior = eachDayOfInterval({ start: addDays(arr[arr.length - 1], 1), end: ultimoDiaDaSemana });
-    }
-    // Botoes de controle para o proximo mes e o mes anterior
-    const recuarMes = format( subMonths(anoMesAtual, 1), 'yyyy-MM-dd');
-    const avancarMes = format( addMonths(anoMesAtual, 1), 'yyyy-MM-dd');
-    //
-    
-    // Obtendo os dias do calendario
-    let diasCalendario = ultimosDiasMesAnterior ? ultimosDiasMesAnterior : [];
-    diasCalendario = diasCalendario.concat(arr).concat(primeirosDiasMesPosterior ? primeirosDiasMesPosterior : []);
+    // Pega os dias até (mas não) a data do primeiro dia do mês
+    ultimosDiasMesAnterior = eachDayOfInterval({
+      start: primeiroDiaSemana,
+      end: subDays(arr[0], 1),
+    });
+  }
+  //  Veja se o ultimo dia do mes é menor que 6
+  if (ultimoDiaMes < 6) {
+    const ultimoDiaDaSemana = addDays(arr[arr.length - 1], 6 - ultimoDiaMes);
+    // Pega os dias até (mas não) a data do ultimo dia do mes
+    primeirosDiasMesPosterior = eachDayOfInterval({
+      start: addDays(arr[arr.length - 1], 1),
+      end: ultimoDiaDaSemana,
+    });
+  }
+  // Botoes de controle para o proximo mes e o mes anterior
+  const recuarMes = format(subMonths(anoMesAtual, 1), "yyyy-MM-dd");
+  const avancarMes = format(addMonths(anoMesAtual, 1), "yyyy-MM-dd");
+  //
 
-    return {
-      recuarMes,
-      avancarMes,
-      diasCalendario,
-      ultimosDiasMesAnterior,
-      primeirosDiasMesPosterior,
-    }
-    
-}
+  // Obtendo os dias do calendario
+  let diasCalendario = ultimosDiasMesAnterior ? ultimosDiasMesAnterior : [];
+  diasCalendario = diasCalendario
+    .concat(arr)
+    .concat(primeirosDiasMesPosterior ? primeirosDiasMesPosterior : []);
+
+  return {
+    recuarMes,
+    avancarMes,
+    diasCalendario,
+    ultimosDiasMesAnterior,
+    primeirosDiasMesPosterior,
+  };
+};
 //
-export default Calendario
+export default Calendario;
--- src/stories/Calendario.stories.jsx
+++ src/stories/Calendario.stories.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import Calendario from '../components/calendario';
-import { Caption } from '../components/tipografia';
-import CrisisAlertIcon from '@mui/icons-material/CrisisAlert';
-
+import React from "react";
+import Calendario from "../components/calendario";
+import { Caption } from "../components/tipografia";
+import CrisisAlertIcon from "@mui/icons-material/CrisisAlert";
+import { Paper, Typography } from "@mui/material";
 
 export default {
-  title: 'Example/Calendario',
+  title: "Example/Calendario",
   component: Calendario,
   parameters: {
-    componentSubtitle: 'Este componente exibe um lindo calendário para manipulação e inclusão de informações (eventos)'
-}
+    componentSubtitle:
+      "Este componente exibe um lindo calendário para manipulação e inclusão de informações (eventos)",
+  },
 };
 
 const Template = (args) => <Calendario {...args} />;
@@ -17,105 +18,137 @@ const Template = (args) => <Calendario {...args} />;
 export const Padrao = Template.bind({});
 
 Padrao.args = {
-  dataInicial: '2022-05-01',    
-}
+  dataInicial: "2022-05-01",
+};
 
 Padrao.story = {
   parameters: {
     docs: {
-        storyDescription: 'Calendario em funcionalidade padrão.'
-    }
-  }
-}
+      storyDescription: "Calendario em funcionalidade padrão.",
+    },
+  },
+};
 
 export const ComRender = Template.bind({});
 
 ComRender.args = {
-  dataInicial: '2022-05-01',
-  render: (dia)=>{
-    if(dia === '2022-05-10') return (
-      <>
-        <CrisisAlertIcon />
-        <Caption title='Meta Conquistada com sucesso absoluto' color='success.main'>Meta Conquistada com sucesso absoluto</Caption>
-      </>
-      
-    )
-  }    
-}
+  dataInicial: "2022-05-01",
+  render: (dia) => {
+    if (dia === "2022-05-10")
+      return (
+        <>
+          <CrisisAlertIcon />
+          <Caption
+            title="Meta Conquistada com sucesso absoluto"
+            color="success.main"
+          >
+            Meta Conquistada com sucesso absoluto
+          </Caption>
+        </>
+      );
+  },
+};
 
 ComRender.story = {
   parameters: {
     docs: {
-        storyDescription: 'Calendario com um dia marcado com a informação.'
-    }
-  }
-}
+      storyDescription: "Calendario com um dia marcado com a informação.",
+    },
+  },
+};
 
 export const ComEstilo = Template.bind({});
 
 ComEstilo.args = {
-  dataInicial: '2022-05-01',
-  renderEstiloDia: (dia)=>{
-    if(dia === '2022-05-10') return {
-      backgroundColor: theme=> theme.palette.primary.main,
-      color: theme=> theme.palette.primary.constrastText,
-    }
-  }    
-}
+  dataInicial: "2022-05-01",
+  renderEstiloDia: (dia) => {
+    if (dia === "2022-05-10")
+      return {
+        backgroundColor: (theme) => theme.palette.primary.main,
+        color: (theme) => theme.palette.primary.constrastText,
+      };
+  },
+};
 
 ComEstilo.story = {
   parameters: {
     docs: {
-        storyDescription: 'Calendario com um dia estilizado para dar destaque sobre o calendário.'
-    }
-  }
-}
+      storyDescription:
+        "Calendario com um dia estilizado para dar destaque sobre o calendário.",
+    },
+  },
+};
 
 export const OnClick = Template.bind({});
 
 OnClick.args = {
-  dataInicial: '2022-05-01',
-  onClick: (dia)=>{
+  dataInicial: "2022-05-01",
+  onClick: (dia) => {
     alert(`Você clicou no dia ${JSON.stringify(dia)}`);
-  }
-}
+  },
+};
 
 OnClick.story = {
   parameters: {
     docs: {
-        storyDescription: 'Calendario com função de onClick ativa, qualquer dia clicado no calendario corresponderá a uma chamada a função.'
-    }
-  }
-}
+      storyDescription:
+        "Calendario com função de onClick ativa, qualquer dia clicado no calendario corresponderá a uma chamada a função.",
+    },
+  },
+};
 
 export const ControleCalendario = Template.bind({});
 
 ControleCalendario.args = {
-  dataInicial: '2022-05-01',
-  onControleCalendario: (priDia, ultDia, controle)=>{
-    alert(`Você clicou no controle ${controle} primerio dia ${priDia} e ultimo dia ${ultDia} deste mês`)
-  }
-}
+  dataInicial: "2022-05-01",
+  onControleCalendario: (priDia, ultDia, controle) => {
+    alert(
+      `Você clicou no controle ${controle} primerio dia ${priDia} e ultimo dia ${ultDia} deste mês`
+    );
+  },
+};
 
 ControleCalendario.story = {
   parameters: {
     docs: {
-        storyDescription: 'Calendario com função de controle sobre o avanço e recuo do calendario. Com isto é possível determinar quando o calendario avançou ou recuou e executar algum tipo de função auxiliar.'
-    }
-  }
-}
+      storyDescription:
+        "Calendario com função de controle sobre o avanço e recuo do calendario. Com isto é possível determinar quando o calendario avançou ou recuou e executar algum tipo de função auxiliar.",
+    },
+  },
+};
 
 export const ComAguardo = Template.bind({});
 
 ComAguardo.args = {
-  dataInicial: '2022-05-01',
+  dataInicial: "2022-05-01",
   aguardar: true,
-}
+};
 
 ComAguardo.story = {
   parameters: {
     docs: {
-        storyDescription: 'Calendario com props aguardar o que impede de interagir com qualquer parte do calendario.'
-    }
-  }
-}
+      storyDescription:
+        "Calendario com props aguardar o que impede de interagir com qualquer parte do calendario.",
+    },
+  },
+};
+
+export const ComRenderModalLateral = Template.bind({});
+
+ComRenderModalLateral.args = {
+  dataInicial: "2022-05-01",
+  renderModalLateral: (dia) => (
+    <Paper sx={{ minHeight: "100%" }}>
+      <Typography variant="h4">Olá, hoje é {dia}</Typography>
+    </Paper>
+  ),
+};
+
+ComRenderModalLateral.story = {
+  parameters: {
+    docs: {
+      storyDescription:
+        "Calendario que quando clicado exibe/oculta um modal lateral customizado",
+    },
+  },
+};
